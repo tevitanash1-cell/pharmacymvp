1962e2304bdebcf857a2c531d40f1e2586256048
tevitanash1-cell <tevitanash1@gmail.com>
2025-11-29 23:33:48 +1000

    Add API endpoint for fetching pharmacies data

---
+++ pages/api/pharmacies.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import pharmacies from '../../data/pharmacies.json';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    res.status(200).json(pharmacies);
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
